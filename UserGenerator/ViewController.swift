65aefe0e162e3bb9803eab72f3ce93067a89e47f
Marat Nazirov <maratnazirov87@gmail.com>
2021-11-17 14:22:52 +0300

    Initial Commit

---
+++ UserGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserGenerator
+//
+//  Created by Marat on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
