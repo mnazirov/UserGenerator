706a0263343e025846b73d8aeff253d64b4a687d
Marat Nazirov <maratnazirov87@gmail.com>
2021-11-18 09:26:15 +0300

    Added UI elements

--- UserGenerator/Views/UserViewController.swift
+++ UserGenerator/Views/UserViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 class UserViewController: UIViewController, UserPresenterDelegate {
     
     private let userPresenter = UserPresenter()
-        
+    
     private var user: User?
-
+    
+    // MARK: - UI elements
+    private let userViewImage = UIImageView()
+    private let firstNameLabel = UILabel()
+    private let lastNameLabel = UILabel()
+    private let gender = UILabel()
+    private let city = UILabel()
+    private let country = UILabel()
+    private let postcode = UILabel()
+    private let email = UILabel()
+    private let username = UILabel()
+    private let password = UILabel()
+    private let phone = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -28,14 +41,14 @@ class UserViewController: UIViewController, UserPresenterDelegate {
     private func setNavigavionBar() {
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Fake persone"
-            
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .refresh,
             target: self,
             action: #selector(addNewTask)
         )
     }
-        
+    
     @objc private func addNewTask() {
         userPresenter.getUsers()
     }
