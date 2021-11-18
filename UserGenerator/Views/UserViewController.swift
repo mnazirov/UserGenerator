4911ca47f7d1b8950cfe91cb4f795c2538690981
Marat Nazirov <maratnazirov87@gmail.com>
2021-11-18 11:11:54 +0300

    Configured the data filling

--- UserGenerator/Views/UserViewController.swift
+++ UserGenerator/Views/UserViewController.swift
@@ -11,9 +11,16 @@ class UserViewController: UIViewController, UserPresenterDelegate {
     
     private let userPresenter = UserPresenter()
     
-    private var user: User?
+    private var user: User? {
+        didSet {
+            DispatchQueue.main.async {
+                self.setupUI()
+            }
+        }
+    }
     
     // MARK: - UI elements
+    private let photoImageView = UIImageView()
     private let userViewImage = UIImageView()
     private let firstNameLabel = UILabel()
     private let lastNameLabel = UILabel()
@@ -32,6 +39,8 @@ class UserViewController: UIViewController, UserPresenterDelegate {
         
         userPresenter.setViewDelegate(delegate: self)
         userPresenter.getUsers()
+        
+        setNavigavionBar()
     }
     
     func presentUser(user: User) {
@@ -49,8 +58,37 @@ class UserViewController: UIViewController, UserPresenterDelegate {
         )
     }
     
+    private func setupUI() {
+        photoImageView.backgroundColor = .systemGray2
+        photoImageView.layer.cornerRadius = 100
+        photoImageView.clipsToBounds = true
+        
+        if let url = user?.picture?.large {
+            guard let imageUrl = URL(string: url) else { return }
+            guard let imageDate = try? Data(contentsOf: imageUrl) else { return }
+                        
+            DispatchQueue.main.async {
+                self.photoImageView.image = UIImage(data: imageDate)
+            }
+        }
+        
+        firstNameLabel.text = "Name: \(user?.name?.first ?? "Unknown")"
+        lastNameLabel.text = "Last name: \(user?.name?.last ?? "Unknown")"
+        gender.text = "Gender: \(user?.gender ?? "Unknown")"
+        city.text = "City: \(user?.location?.city ?? "Unknown")"
+        country.text = "Country: \(user?.location?.country ?? "Unknown")"
+        postcode.text = "Postcode: \(user?.location?.postcode ?? 0)"
+        email.text = "Email: \(user?.email ?? "Unknown")"
+        username.text = "Username: \(user?.login?.username ?? "Unknown")"
+        password.text = "Password: \(user?.login?.password ?? "Unknown")"
+        phone.text = "Phone: \(user?.phone ?? "Unknown")"
+        
+        view.setNeedsLayout()
+    }
+    
     @objc private func addNewTask() {
         userPresenter.getUsers()
+        setupUI()
     }
 }
 
