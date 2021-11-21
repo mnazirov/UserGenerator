a6dbec9baa058c66ece5d209425be6b6e67645fa
Marat Nazirov <maratnazirov87@gmail.com>
2021-11-21 15:05:24 +0300

    Removed the user field and inside the presentUser method to update the UI with new data

--- UserGenerator/Views/UserViewController.swift
+++ UserGenerator/Views/UserViewController.swift
@@ -11,14 +11,6 @@ class UserViewController: UIViewController, UserPresenterDelegate {
     
     private let userPresenter = UserPresenter()
     
-    private var user: User? {
-        didSet {
-            DispatchQueue.main.async {
-                self.setupUI()
-            }
-        }
-    }
-    
     // MARK: - UI elements
     private let photoImageView = UIImageView()
     private let userViewImage = UIImageView()
@@ -45,7 +37,31 @@ class UserViewController: UIViewController, UserPresenterDelegate {
     }
     
     func presentUser(user: User) {
-        self.user = user
+        let user = user
+        
+        if let url = user.picture?.large {
+            guard let imageUrl = URL(string: url) else { return }
+            guard let imageDate = try? Data(contentsOf: imageUrl) else { return }
+            
+            DispatchQueue.main.async {
+                self.photoImageView.image = UIImage(data: imageDate)
+            }
+        }
+        
+        DispatchQueue.main.async {
+            self.firstNameLabel.text = "First name: \(user.name?.first ?? "Unknown")"
+            self.lastNameLabel.text = "Last name: \(user.name?.last ?? "Unknown")"
+            self.gender.text = "Gender: \(user.gender ?? "Unknown")"
+            self.city.text = "City: \(user.location?.city ?? "Unknown")"
+            self.country.text = "Country: \(user.location?.country ?? "Unknown")"
+            self.postcode.text = "Postcode: \(user.location?.postcode ?? 0)"
+            self.email.text = "Email: \(user.email ?? "Unknown")"
+            self.username.text = "Username: \(user.login?.username ?? "Unknown")"
+            self.password.text = "Password: \(user.login?.password ?? "Unknown")"
+            self.phone.text = "Phone: \(user.phone ?? "Unknown")"
+            
+            self.view.setNeedsLayout()
+        }
     }
     
     // MARK: - Private methods
@@ -60,59 +76,33 @@ class UserViewController: UIViewController, UserPresenterDelegate {
         )
     }
     
-    private func setupUI() {
+    private func setupLayout() {
         photoImageView.backgroundColor = .systemGray2
         photoImageView.layer.cornerRadius = 100
         photoImageView.clipsToBounds = true
         
-        if let url = user?.picture?.large {
-            guard let imageUrl = URL(string: url) else { return }
-            guard let imageDate = try? Data(contentsOf: imageUrl) else { return }
-                        
-            DispatchQueue.main.async {
-                self.photoImageView.image = UIImage(data: imageDate)
-            }
-        }
+        let stackView = UIStackView(arrangedSubviews: [photoImageView, firstNameLabel,
+                                                       lastNameLabel, gender, city, country,
+                                                       postcode, email, username, password, phone])
+        stackView.spacing = 15
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        stackView.setCustomSpacing(30, after: photoImageView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
         
-        firstNameLabel.text = "First name: \(user?.name?.first ?? "Unknown")"
-        lastNameLabel.text = "Last name: \(user?.name?.last ?? "Unknown")"
-        gender.text = "Gender: \(user?.gender ?? "Unknown")"
-        city.text = "City: \(user?.location?.city ?? "Unknown")"
-        country.text = "Country: \(user?.location?.country ?? "Unknown")"
-        postcode.text = "Postcode: \(user?.location?.postcode ?? 0)"
-        email.text = "Email: \(user?.email ?? "Unknown")"
-        username.text = "Username: \(user?.login?.username ?? "Unknown")"
-        password.text = "Password: \(user?.login?.password ?? "Unknown")"
-        phone.text = "Phone: \(user?.phone ?? "Unknown")"
-        
-        view.setNeedsLayout()
+        NSLayoutConstraint.activate([
+            photoImageView.heightAnchor.constraint(equalToConstant: 200),
+            photoImageView.widthAnchor.constraint(equalToConstant: 200),
+            
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
     }
-    
-    private func setupLayout() {
-            
-            let stackView = UIStackView(arrangedSubviews: [photoImageView, firstNameLabel,
-                                                           lastNameLabel, gender, city, country,
-                                                           postcode, email, username, password, phone])
-            stackView.spacing = 15
-            stackView.axis = .vertical
-            stackView.alignment = .leading
-            stackView.setCustomSpacing(30, after: photoImageView)
-            stackView.translatesAutoresizingMaskIntoConstraints = false
-            view.addSubview(stackView)
-            
-            NSLayoutConstraint.activate([
-                photoImageView.heightAnchor.constraint(equalToConstant: 200),
-                photoImageView.widthAnchor.constraint(equalToConstant: 200),
-                
-                stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-                stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            ])
-        }
     
     @objc private func addNewTask() {
         userPresenter.getUsers()
-        setupUI()
     }
 }
 
